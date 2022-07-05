9527a33517ae28c3cf1c8f34e166866f9705712a
Exaedro <owomolo123@gmail.com>
2022-07-05 20:41:20 -0300

    Updated script.js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-setTimeout(() => {
+setInterval(() => {
     const elements = document.getElementsByClassName('dashboard-card-footer');
     
     for(var i = 0;i < elements.length; i++) {
         elements[i].classList.remove('bg-white');
     } 
-}, 2000)
+}, 500)
         
    
